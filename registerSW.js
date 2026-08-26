3e305f3735ce0a45912ef59c5fe40d737bd490f5
Benson-LU77 <295537940+Benson-LU77@users.noreply.github.com>
2026-08-26 17:45:11 +0800

    deploy 夜雨 FM

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tata/sw.js', { scope: '/tata/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
